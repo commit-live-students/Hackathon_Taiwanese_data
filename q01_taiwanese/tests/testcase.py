873059daa8c376cf8ed6ddbdd793c8535f681634
Alex19427 <alex19427@gmail.com>
2018-02-21 18:14:10 +0530

    update

--- q01_taiwanese/tests/testcase.py
+++ q01_taiwanese/tests/testcase.py
@@ -1,17 +1,12 @@
-
 import unittest
 import pandas as pd
 from pandas.util.testing import assert_frame_equal
 
+original_return = pd.read_csv('Original_classes.csv',header=None)
+student_return = pd.read_csv('../predicted_class.csv',header=None)
+
 
 class Testing(unittest.TestCase):
-	def setUp(self):
-	    self.path = '../predicted_class.csv'
-	    self.student_return = pd.read_csv(self.path,header=None)
-	    self.original_return = pd.read_csv('Original_classes.csv',header=None)	
-
 	def test_return(self):
-            assert_frame_equal(self.student_return, self.original_return, "Return value shape does not match expected value")
-	
-if __name__=='__main__':
-    unittest.main()
+            assert_frame_equal(len(student_return), len(original_return), "Return value shape does not match expected value")
+
